ad85287c8df4d82748cba0bc282164cde573893e
remisany <remi.d.sany@gmail.com>
2021-09-09 18:43:59 +0200

    Ajout dossier styles

---
+++ hrnet/src/styles/colors.js
@@ -0,0 +1,14 @@
+const colors = {
+    white: "#FCFCFD",
+    blue: "#3233D3",
+    dark: "#05060A",
+    grey1: "#A4ADC5",
+    grey2: "#8E97B0",
+    grey3: "#7C86A1",
+    darkgrey: "#6e6e6e",
+    orange: "#F04329",
+    purpleDark: "#323161",
+    purpleLight: "#594B94"
+}
+
+export default colors

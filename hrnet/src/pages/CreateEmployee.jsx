038adb4a199d568e1deb8888adfee9f81a632da1
remisany <remi.d.sany@gmail.com>
2021-09-14 22:00:15 +0200

    Ajout modal (npm)

--- hrnet/src/pages/CreateEmployee.jsx
+++ hrnet/src/pages/CreateEmployee.jsx
@@ -1,4 +1,6 @@
 import styled from "styled-components"
+import {useState} from "react"
+import {Modal} from "rs-react-modal";
 
 //Components
 import SimpleInput from "../components/SimpleInput"
@@ -9,7 +11,6 @@ import colors from "../styles/colors"
 const FORM = styled.form`
     display: flex;
     flex-direction: column;
-
     row-gap: 2.5rem;
 `
 
@@ -24,9 +25,50 @@ const BUTTON = styled.button`
     background-color: ${colors.orange};
     color: ${colors.white};
 `
+const customStyle = {
+    content : {
+        fontSize: "1.5rem",
+        color: colors.purpleLight,
+        position: "relative",
+        display: "flex",
+        flexDirection: "column",
+        rowGap: "2rem",
+        margin: "25rem auto",
+        maxWidth: "50rem",
+        background: colors.white,
+        padding: "2rem",
+        alignItems: "center",
+        borderRadius: ".3rem"
+      },
 
+    button : {
+        fontSize: "2rem",
+        cursor: "pointer",
+        fontWeight: "bold",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        background: colors.orange,
+        color: colors.white,
+        borderColor: colors.orange,
+        width: "12rem",
+        padding: "1rem",
+        borderRadius: ".3rem"
+    }
+}
 
 function CreateEmployee () {
+    const [active, setActive] = useState(false)
+
+    const close = () => {
+        setActive(!active)
+    }
+
+    const handleSumbit = (e) => {
+        e.preventDefault()
+        setActive(true)
+    }
+
     return (
         <main>
             <FORM>
@@ -50,8 +92,14 @@ function CreateEmployee () {
                     type = "number"
                     id = "zip"
                 />
-                <BUTTON>Save</BUTTON>
+                <BUTTON onClick = {(e) => handleSumbit(e)}>Save</BUTTON>
             </FORM>
+            <Modal
+                open = {active}
+                modalContent = "Employee Created!"
+                close = {close}
+                style = {customStyle}
+            />
         </main>
     )
 }

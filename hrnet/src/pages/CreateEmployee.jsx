4f5ab578d1f0a5ada68422e2f953b5025f1017b6
remisany <remi.d.sany@gmail.com>
2021-09-14 17:49:21 +0200

    Design page

--- hrnet/src/pages/CreateEmployee.jsx
+++ hrnet/src/pages/CreateEmployee.jsx
@@ -1,10 +1,35 @@
+import styled from "styled-components"
+
 //Components
 import SimpleInput from "../components/SimpleInput"
+
+//Styles
+import colors from "../styles/colors"
+
+const FORM = styled.form`
+    display: flex;
+    flex-direction: column;
+
+    row-gap: 2.5rem;
+`
+
+const BUTTON = styled.button`
+    padding: 1rem;
+    font-size: 2rem;
+    font-weight: bold;
+    margin-top: 1rem;
+    border-color: ${colors.orange};
+    cursor: pointer;
+    border-radius: .3rem;
+    background-color: ${colors.orange};
+    color: ${colors.white};
+`
+
 
 function CreateEmployee () {
     return (
         <main>
-            <form>
+            <FORM>
                 <SimpleInput
                     type = "text"
                     id = "first-name"
@@ -25,7 +50,8 @@ function CreateEmployee () {
                     type = "number"
                     id = "zip"
                 />
-            </form>
+                <BUTTON>Save</BUTTON>
+            </FORM>
         </main>
     )
 }

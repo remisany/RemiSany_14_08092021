26eef4eb2839c1723ab03ace8bdd572b81b6d2d4
remisany <remi.d.sany@gmail.com>
2021-09-14 17:48:28 +0200

    Ajout couleurs

--- hrnet/src/styles/colors.js
+++ hrnet/src/styles/colors.js
@@ -1,11 +1,5 @@
 const colors = {
-    white: "#FCFCFD",
-    blue: "#3233D3",
-    dark: "#05060A",
-    grey1: "#A4ADC5",
-    grey2: "#8E97B0",
-    grey3: "#7C86A1",
-    darkgrey: "#6e6e6e",
+    white: "#F8F7FB",
     orange: "#F04329",
     purpleDark: "#323161",
     purpleLight: "#594B94"

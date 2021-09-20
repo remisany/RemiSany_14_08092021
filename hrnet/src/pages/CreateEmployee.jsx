cdb60f745992b34ab170c491f63db1ad1b28f621
remisany <remi.d.sany@gmail.com>
2021-09-20 19:13:31 +0200

    Ajout composant à la page

--- hrnet/src/pages/CreateEmployee.jsx
+++ hrnet/src/pages/CreateEmployee.jsx
@@ -1,9 +1,11 @@
 import styled from "styled-components"
 import {useState} from "react"
-import {Modal} from "rs-react-modal";
 
 //Components
 import SimpleInput from "../components/SimpleInput"
+import SelectStates from "../components/SelectStates"
+import SelectDepartments from "../components/SelectDepartments"
+import ModalWindow from "../components/ModalWindow"
 
 //Styles
 import colors from "../styles/colors"
@@ -25,37 +27,6 @@ const BUTTON = styled.button`
     background-color: ${colors.orange};
     color: ${colors.white};
 `
-const customStyle = {
-    content : {
-        fontSize: "1.5rem",
-        color: colors.purpleLight,
-        position: "relative",
-        display: "flex",
-        flexDirection: "column",
-        rowGap: "2rem",
-        margin: "25rem auto",
-        maxWidth: "50rem",
-        background: colors.white,
-        padding: "2rem",
-        alignItems: "center",
-        borderRadius: ".3rem"
-      },
-
-    button : {
-        fontSize: "2rem",
-        cursor: "pointer",
-        fontWeight: "bold",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        background: colors.orange,
-        color: colors.white,
-        borderColor: colors.orange,
-        width: "12rem",
-        padding: "1rem",
-        borderRadius: ".3rem"
-    }
-}
 
 function CreateEmployee () {
     const [active, setActive] = useState(false)
@@ -92,13 +63,13 @@ function CreateEmployee () {
                     type = "number"
                     id = "zip"
                 />
+                <SelectStates />
+                <SelectDepartments />
                 <BUTTON onClick = {(e) => handleSumbit(e)}>Save</BUTTON>
             </FORM>
-            <Modal
-                open = {active}
-                modalContent = "Employee Created!"
+            <ModalWindow
+                active = {active}
                 close = {close}
-                style = {customStyle}
             />
         </main>
     )

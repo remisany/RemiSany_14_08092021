757115732b7efaf85cd969ffe5992916242aafc6
remisany <remi.d.sany@gmail.com>
2021-09-21 13:05:16 +0200

    MAJ modal version

--- hrnet/src/components/ModalWindow.jsx
+++ hrnet/src/components/ModalWindow.jsx
@@ -4,36 +4,34 @@ import {Modal} from "rs-react-modal"
 import colors from "../styles/colors"
 
 function ModalWindow ({ active, close }) {
-    const customStyle = {
-        content : {
-            fontSize: "1.5rem",
-            color: colors.purpleLight,
-            position: "relative",
-            display: "flex",
-            flexDirection: "column",
-            rowGap: "2rem",
-            margin: "25rem auto",
-            maxWidth: "50rem",
-            background: colors.white,
-            padding: "2rem",
-            alignItems: "center",
-            borderRadius: ".3rem"
-          },
+    const customContent = {
+        fontSize: "1.5rem",
+        color: colors.purpleLight,
+        position: "relative",
+        display: "flex",
+        flexDirection: "column",
+        rowGap: "2rem",
+        margin: "25rem auto",
+        maxWidth: "50rem",
+        background: colors.white,
+        padding: "2rem",
+        alignItems: "center",
+        borderRadius: ".3rem"
+    }
     
-        button : {
-            fontSize: "2rem",
-            cursor: "pointer",
-            fontWeight: "bold",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            background: colors.orange,
-            color: colors.white,
-            borderColor: colors.orange,
-            width: "12rem",
-            padding: "1rem",
-            borderRadius: ".3rem"
-        }
+    const customButton = {
+        fontSize: "2rem",
+        cursor: "pointer",
+        fontWeight: "bold",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        background: colors.orange,
+        color: colors.white,
+        borderColor: colors.orange,
+        width: "12rem",
+        padding: "1rem",
+        borderRadius: ".3rem"
     }
 
     return (
@@ -41,7 +39,8 @@ function ModalWindow ({ active, close }) {
             open = {active}
             modalContent = "Employee Created!"
             close = {close}
-            style = {customStyle}
+            styleButton= {customButton}
+            styleContent = {customContent}
         />
     )
 }

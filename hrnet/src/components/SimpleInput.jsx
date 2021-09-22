506d36ee43213e74120d20b712982e429c31d30e
remisany <remi.d.sany@gmail.com>
2021-09-22 18:59:04 +0200

    Simplification style

--- hrnet/src/components/SimpleInput.jsx
+++ hrnet/src/components/SimpleInput.jsx
@@ -1,44 +1,9 @@
 import { useState } from "react"
 import styled from "styled-components"
 
-//Styles
-import colors from "../styles/colors"
-
 const CONTAINER = styled.div`
     display: flex;
     flex-direction: column;
-
-    label {
-        position: absolute;
-        color: ${colors.orange};
-        font-size: 1.3rem;
-        margin-top: -1.4rem;
-        font-weight: 700;
-    }
-
-    input {
-        font-size: 1.5rem;
-        padding: .2rem 0;
-        border: none;
-        background-color: #F8F7FB;
-        border-bottom: solid .2rem ${colors.purpleLight};
-        color: ${colors.purpleLight};
-        font-weight: 500;
-
-        ::placeholder {
-            color: ${colors.purpleLight};
-            font-weight: 300;
-        }
-
-        :focus {
-            outline: none;
-            border-bottom: solid .2rem ${colors.orange};
-
-            ::placeholder {
-                color: transparent;
-            }
-        }
-    }
 `
 
 function SimpleInput ({ type, id }) {
